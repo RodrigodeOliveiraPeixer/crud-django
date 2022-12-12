896526248510e947b9085bf35929f8be1e54cb66
RodrigodeOliveiraPeixer <109769171+RodrigodeOliveiraPeixer@users.noreply.github.com>
2022-12-11 21:57:14 -0300

    Update views.py

--- core/views.py
+++ core/views.py
@@ -5,27 +5,27 @@ from .models import Pessoa
 
 def home(request):
     pessoas = Pessoa.objects.all()
-    return render(request, "index.html", {"pessoas": pessoas})
+    return render(request, "index.html", {"pessoas": pessoas}
 
 def salvar(request):
     vnome = request.POST.get("nome")
     Pessoa.objects.create(nome=vnome)
     pessoas = Pessoa.objects.all()
-    return render(request, "index.html", {"pessoas": pessoas})
+    return render(request, "index.html", {"pessoas": pessoas}
 
 def editar(request, id):
     pessoa = Pessoa.objects.get(id=id)
-    return render(request, "update.html", {"pessoa": pessoa})
+    return render(request, "update.html", {"pessoa": pessoa}
 
 def update(request, id):
     vnome = request.POST.get("nome")
-    pessoa = Pessoa.objects.get(id=id)
+    pessoa = Pessoa.objects.get(id=id
     pessoa.nome = vnome
     pessoa.save()
     return redirect(home)
 
 def delete(request, id):
-    pessoa = Pessoa.objects.get(id=id)
+    pessoa = Pessoa.objects.get(id=id
     pessoa.delete()
     return redirect(home)
 
